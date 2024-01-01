1a7ed5aabc8856d5d05c94a0967c3270e64ed768
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-01-01 11:33:05 +0545

    Update primary_&_foreign_key.sql

--- DBMS college/primary_&_foreign_key.sql
+++ DBMS college/primary_&_foreign_key.sql
@@ -1,38 +1,38 @@
 
 C:\Users\user>sqlplus hr/hr
 
-SQL Plus: Release 11.2.8.2.8 Production on Sun Dec 31 08:15:28 2023
+-- SQL Plus: Release 11.2.8.2.8 Production on Sun Dec 31 08:15:28 2023
 
-Copyright (c) 1982, 2014, Oracle. All rights reserved.
+-- Copyright (c) 1982, 2014, Oracle. All rights reserved.
 
-Connected to:
+-- Connected to:
 
-Oracle Database 11g Express Edition Release 11.2.0.2.8 64bit Production
+-- Oracle Database 11g Express Edition Release 11.2.0.2.8 64bit Production
 
 SQL> drop table student
 
 2 SQL> DROP TABLE STUDENT;
 
-Table dropped.
+--Table dropped.
 
 
 
-SQL> CREATE TABLE DUMMY STUDENT(SID NUMBER<S> PRIMARY KEY, NAME VARCHAR<50> NOT
+SQL> CREATE TABLE DUMMY STUDENT(SID NUMBER<S> PRIMARY KEY,
+ NAME VARCHAR (50) NOT NULL, 
+ ADDRESS VARCHAR(50) NOT NULL);
 
-NULL, ADDRESS VARCHAR(50) NOT NULL);
+--Table created.
 
-Table created.
+SQL> CREATE TABLE DUMMY FEES(FID NUMBER(5),
 
-SQL> CREATE TABLE DUMMY FEES(FID NUMBER<5>,
+     SID NUMBER(5) NOT NULL,
 
-     SID NUMBER<5> NOT NULL,
-
-     AMOUNT PAID NUMBER<5>,
+     AMOUNT PAID NUMBER(5),
 
      PRIMARY KEY (PID),
 
-     POREIGN KEY (SID) REFERENCES DUMMY_STUDENT (SID>>;
+     POREIGN KEY (SID) REFERENCES DUMMY_STUDENT (SID));
 
-Table created.
+--Table created.
 
 SQL> DESC DUMMY FEES; 
