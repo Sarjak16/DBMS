382fe9910c8d3444cd8f0e378ef14876ef1d904b
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-01-16 22:15:04 +0545

    shutting db instance

--- oracle database explorer/start&shut_DB_instances.sql
+++ oracle database explorer/start&shut_DB_instances.sql
@@ -15,6 +15,21 @@
     NOTE: STARTUP OPEN COMMAND OPENS IN A CDB ROOT Container IN READ AND WRITE MODE , 
         AND IN PDB$SEED MODE IN READ ONLY MODE.
 
-        
+    4. MOUNT = user created PDBs STAY HERE.
+
+
 
  SQL> SHUTDOWN
+    there are four shutdown modes:
+
+    1. NORMAL: default mode
+                slowest mode
+                waits all sessions to logout.
+    2. IMMEDIATE: terminates and rollback all transactions followed by checkpoint.
+
+    3. TRANSACTIONAL: wait for all transactions which are runinng to complete and then shutdown.
+
+    4. ABORT: immediate and fastest shutdown
+                doesnt wait and  force shutdown.
+                this is done if any shutting mode donot work.
+                
