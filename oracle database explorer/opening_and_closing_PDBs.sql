caecfc015981d66e2d621b0a9e2600d188fa0c01
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-01-23 21:14:42 +0545

    alter

---
+++ oracle database explorer/opening_and_closing_PDBs.sql
@@ -0,0 +1,11 @@
+
+
+-- ---------------------------------------Opening and Closing PDBs
+
+-- Open/close a PDB to open/close its data files.
+
+-- Use the ALTER PLUGGABLE DATABASE command or STARTUP and SHUTDOWN commands to open and close PDBs:
+
+SQL> ALTER PLUGGABLE DATABASE PDB1 OPEN;
+
+-- ALTER PLUGGABLE DATABASE  command lets you change from any open mode to another.
