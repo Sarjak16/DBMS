f8cd77d1e6c37219dd454277620566dd60d6b59a
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-02-02 23:11:42 +0545

    Update opening-and-closing-pdbs-demo.sql

--- oracle database explorer/opening-and-closing-pdbs-demo.sql
+++ oracle database explorer/opening-and-closing-pdbs-demo.sql
@@ -1,25 +1,26 @@
 [oracle@edvmr1p0 ~]$ oraenv
 
-ORACLE SID = [orclcdb] 7
+-- ORACLE SID = [orclcdb] 7
 
-The Oracle base remains unchanged with value /u01/app/oracle [oracle@edvmrlp0 -]$ sqlplus / as sysdba
+--The Oracle base remains unchanged with value /u01/app/oracle 
+[oracle@edvmrlp0 -]$ sqlplus / as sysdba
 
-SQL Plus: Release 19.0.0.0.0 Production on Tue Aug 4 08:00:42 2020
+-- SQL Plus: Release 19.0.0.0.0 Production on Tue Aug 4 08:00:42 2020
 
-Version 19.3.0.0.0
+-- Version 19.3.0.0.0
 
-Copyright (c) 1982, 2019, Oracle. All rights reserved.
+-- Copyright (c) 1982, 2019, Oracle. All rights reserved.
 
-Connected to an idle instance.
+-- Connected to an idle instance.
 
 SQL> startup nomount
 
-ORACLE instance started.
+-- ORACLE instance started.
 
-Total System Global Area    2013264224 bytes
-Fixed Size  9136480 bytes
-Variable Size   620756992 bytes
-Database Buffers    1375731712 bytes
+-- Total System Global Area    2013264224 bytes
+-- Fixed Size  9136480 bytes
+-- Variable Size   620756992 bytes
+-- Database Buffers    1375731712 bytes
 
 SQL> alter database mount;
 
@@ -35,23 +36,16 @@ Database altered.
 
 SQL> show pdbs
 
-CON ID CON NAME
+-- CON ID CON NAME     OPEN MODE       RESTRICTED
+-- ..............      .........       ...........
 
-2 PDB$SEED
+-- 2 PDB$SEED          READ ONLY        NO
 
-3 ORCLPDB1
+-- 3 ORCLPDB1          MOUNTED
 
-4 ORCLPDB2
+-- 4 ORCLPDB2          MOUNTED
 
 SQL> @state
-
-OPEN MODE RESTRICTED
-
-READ ONLY NO
-
-MOUNTED
-
-MOUNTED
 
 SQL> col con name for a30
 
