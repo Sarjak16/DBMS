4fa570785948a97d25724e2b320a65aed8285937
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-02-29 21:52:35 +0545

    Update basicsDBA.sql

---
+++ oracle_sql_explorer/basicsDBA.sql
@@ -0,0 +1,9 @@
+-- User Management
+-- Creating a user
+-- The syntax for creating a user is:
+   CREATE USER username IDENTIFIED BY password;
+For example:
+
+   CREATE USER brian IDENTIFIED BY brianpass;
+
+   
