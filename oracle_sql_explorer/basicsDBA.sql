18abbae1f5038ccc1d47fb776d95eac8d3bf83f0
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-03-06 21:02:27 +0545

    Update basicsDBA.sql

--- oracle_sql_explorer/basicsDBA.sql
+++ oracle_sql_explorer/basicsDBA.sql
@@ -23,6 +23,24 @@ For example:
    ALTER USER brian IDENTIFIED BY brianpassword;
 
 -- Importing and exporting
+
 -- There are two methods of backing up and restoring database tables and data. The 'exp' and 'imp' tools are simpler tools geared towards smaller databases.
 --  If database structures become more complex or are very large ( > 50 GB for example) then using the RMAN tool is more appropriate.
 
+-- Import a dump file using IMP
+
+-- This command is used to import Oracle tables and table data from a *.dmp file created by the 'exp' tool.
+--  Remember that this a command that is executed from the command line through $ORACLE_HOME/bin and not within SQL*Plus.
+
+-- The syntax for importing a dump file is:
+
+   imp KEYWORD=value
+
+   There are number of parameters you can use for keywords.
+
+To view all the keywords:
+
+   imp HELP=yes
+An example:
+
+   imp brian/brianpassword FILE=mydump.dmp FULL=yes
