d29cad9a225900dcb9ab0181ff5ee50e4e368b71
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2024-03-12 21:59:05 +0545

    Update connectsql.py

--- DBMS using python/connectsql.py
+++ DBMS using python/connectsql.py
@@ -10,3 +10,23 @@ mydb = mysql.connector.connect(
  
 # Printing the connection object 
 print(mydb)
+
+
+#.............................................................................................................................
+ 
+# Python program to connect
+# to mysql database
+ 
+ 
+from mysql.connector import connection
+ 
+# Connecting to the server
+conn = connection.MySQLConnection(user = 'username', 
+                              host = 'localhost',
+                              database = 'database_name')
+ 
+print(conn)
+ 
+# Disconnecting from the server
+conn.close()
+
